e8d4acec4bfceaf3c3dbd5f1fbaced77c2d829f2
geovanyArenas <145078589+geo1605@users.noreply.github.com>
2024-11-23 23:02:45 -0600

    commit final

--- proyectoUTDdjango/settings.py
+++ proyectoUTDdjango/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +80,8 @@ WSGI_APPLICATION = 'proyectoUTDdjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
